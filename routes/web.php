d46912aa87773fdede29b09cada3797ae4d6ab64
hninshwesin <hninshwesinphyo.mm@gmai.com>
2021-02-17 11:20:08 +0630

    delete admin user testing code

--- routes/web.php
+++ routes/web.php
@@ -29,13 +29,3 @@ Route::resource('patient','PatientController');
 
 Route::resource('assign', 'AssignController');
 
-// Admin routes
-Route::prefix('admin')->group(function(){
-    Route::get('/', 'Admin\AdminController@index')->name('admin.dashboard');
-    Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-    Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/logout', 'Auth\AdminLoginController@logout')->name('admin.logout');
-    Route::get('/register', 'Auth\AdminRegisterController@showRegisterForm')->name('admin.register');
-    Route::post('/register', 'Auth\AdminRegisterController@register')->name('admin.register.submit');
-});
-
