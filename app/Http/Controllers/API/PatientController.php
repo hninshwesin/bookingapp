352268f4364235e02a65da15f17c2d03658ad843
hninshwesin <hninshwesinphyo.mm@gmai.com>
2021-02-21 18:41:15 +0630

    search patient api

--- app/Http/Controllers/API/PatientController.php
+++ app/Http/Controllers/API/PatientController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PatientResource;
+use App\Http\Resources\SearchPatientResource;
+use App\Http\Resources\SearchPatientResourceCollection;
+use App\Http\Resources\WaitingResource;
 use App\Patient;
+use App\WaitingList;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,4 +21,31 @@ class PatientController extends Controller
 
         return new PatientResource($patient);
     }
+
+    public function searchpatient(Request $request)
+    {
+        $data = $request->get('name');
+        $doctor = Auth::guard('doctor-api')->user();
+
+//        $patient = WaitingList::with('patient')->where('doctor_id', $doctor->id)->get();
+
+//        $patient = WaitingList::with('patient')
+//            ->where('doctor_id', $doctor->id)
+//            ->whereHas('patient', function ($query) use ($data){
+//            $query->where('Name', 'like', '%' . $data . '%')->get();
+//        });
+
+
+        $patient = WaitingList::with(['patient' => function ($query) use ($data) {$query->where('Name', 'like', '%' . $data . '%');}])
+            ->where('doctor_id', $doctor->id)
+            ->get();
+
+        return new SearchPatientResourceCollection($patient);
+
+
+
+    }
 }
+
+
+//'like', '%' . $data . '%'
--- routes/api.php
+++ routes/api.php
@@ -37,9 +37,10 @@ Route::group(['prefix' => 'v1'], function () {
         Route::get('waiting', 'API\WaitingListAndPatientListController@waiting');
         Route::post('patient_visit/{patient_id}', 'API\VisitController@store');
         Route::get('patient_detail/{patient_id}', 'API\VisitController@detail');
-        Route::get('patient/{patient_id}', 'API\PatientController@patient');
         Route::post('referral', 'API\ReferralController@store');
         Route::get('doctors', 'API\ReferralController@doctors');
+        Route::get('patient/{patient_id}', 'API\PatientController@patient');
+        Route::get('search', 'API\PatientController@searchpatient');
     });
 });
 
