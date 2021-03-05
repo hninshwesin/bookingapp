4b6c59115d141c8a1f8b659a80e6f166ab5d64a6
hninshwesin <hninshwesinphyo.mm@gmai.com>
2021-03-05 22:19:04 +0630

    add some column in doctor get field

--- app/Http/Resources/DoctorResource.php
+++ app/Http/Resources/DoctorResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class DoctorResource extends JsonResource
@@ -17,10 +18,18 @@ class DoctorResource extends JsonResource
         return [
             'id' => $this->id,
             'Name' => $this->Name,
+            'sama_number' => $this->sama_number,
             'Qualifications' => $this->Qualifications,
             'Contact_Number' => $this->Contact_Number,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'start_time' => Carbon::parse($this->start_time)->format('g:i A'),
+            'end_time' => Carbon::parse($this->end_time)->format('g:i A'),
             'Email' => $this->Email,
-            'File' => new DoctorCertificateCollection($this->DoctorCertificateFile),
+            'other_option' => $this->other_option,
+            'Certificate_File' => new DoctorCertificateCollection($this->DoctorCertificateFile),
+            'Profile_image' => new DoctorProfilePictureResource($this->DoctorProfilePicture),
+            'SaMa_or_NRC' => new DoctorSaMaOrNRCResourceCollection($this->DoctorSamaFileOrNrcFile),
 //            'name' => new DoctorCertificateCollection($this->DoctorCertificateFile()->name),
 //            'certificate_file' => new DoctorCertificateCollection($this->DoctorCertificateFile()->certificate_file),
         ];
