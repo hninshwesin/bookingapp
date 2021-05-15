cf145eeaaad560a377810d962e9db12af3ba0ec2
hninshwesin <hninshwesinphyo.mm@gmai.com>
2021-05-15 15:05:06 +0630

    id to app_user_id change in AppUserResource

--- app/Http/Resources/AppUserResource.php
+++ app/Http/Resources/AppUserResource.php
@@ -15,7 +15,7 @@ class AppUserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
+            'app_user_id' => $this->id,
             'name' => $this->name,
             'status' => 0,
             'error_code' => 0
