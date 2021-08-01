2795a74f0e03de52c05ffc6b191fb09b5d01929b
hninshwesin <hninshwesinphyo.mm@gmai.com>
2021-08-01 20:30:38 +0630

    admin panel menu icon change

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,32 +6,32 @@
     </a>
 
     <a href="{{ route('doctor.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-user"></i>
+        <i class="nav-icon fas fa-user-md"></i>
         <p>Doctor List</p>
     </a>
 
     <a href="{{ route('patient.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-user-alt"></i>
+        <i class="nav-icon fas fa-users"></i>
         <p>Patient List</p>
     </a>
 
     <a href="{{ route('ambulance.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-ambulance"></i>
         <p>Ambulance List</p>
     </a>
 
     <a href="{{ route('clinic.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-hospital"></i>
         <p>Clinic List</p>
     </a>
 
     <a href="{{ route('lab.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-heartbeat"></i>
         <p>Lab List</p>
     </a>
 
     <a href="{{ route('pharmacy.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-plus-square"></i>
         <p>Pharmacy List</p>
     </a>
 
@@ -40,32 +40,32 @@
         <p>Specialization List</p>
     </a>
     <a href="{{ route('doctor_approve') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-arrow-right"></i>
         <p>Approve Doctor</p>
     </a>
     <a href="{{ route('ambulance') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-arrow-right"></i>
         <p>Approve Ambulance</p>
     </a>
     <a href="{{ route('clinic') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-arrow-right"></i>
         <p>Approve Clinic</p>
     </a>
     <a href="{{ route('lab') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-arrow-right"></i>
         <p>Approve Lab</p>
     </a>
     <a href="{{ route('pharmacy') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-arrow-right"></i>
         <p>Approve Pharmacy</p>
     </a>
     <a href="{{ route('language.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-language"></i>
         <p>Language List</p>
     </a>
 
     <a href="{{ route('terms_of_reference.index') }}" class="nav-link active">
-        <i class="nav-icon fas fa-stethoscope"></i>
+        <i class="nav-icon fas fa-sticky-note"></i>
         <p>Terms Of Reference</p>
     </a>
 </li>
