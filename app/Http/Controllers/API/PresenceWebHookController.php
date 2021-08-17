fa47b74c35098f9ce528e8ebebc90bbe3ca285b9
hninshwesin <hninshwesinphyo.mm@gmai.com>
2021-08-17 16:30:37 +0630

    presence webhook

---
+++ app/Http/Controllers/API/PresenceWebHookController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\PresenceRawLog;
+use Illuminate\Http\Request;
+
+class PresenceWebHookController extends Controller
+{
+    public function store(Request $request)
+    {
+        $data = PresenceRawLog::create([
+            'body' => $request->getContent()
+        ]);
+
+        return response()->json(['error_code' => 0], 200);
+    }
+}
---
+++ app/PresenceRawLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PresenceRawLog extends Model
+{
+    protected $guarded = [];
+}
